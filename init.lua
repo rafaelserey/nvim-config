7e80d5e6dd8d87e06b993aa6cbba4e569561885d
Rafael Serey <serey.rafael@gmail.com>
2025-06-24 19:13:26 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
+-- init.lua
+
+-- Configurações básicas
 vim.g.lazyvim_check_order = false
 vim.opt.clipboard = "unnamedplus"
 vim.g.lazyvim_disable_startup_message = true
@@ -5,7 +8,20 @@ vim.wo.relativenumber = true
 vim.wo.signcolumn = "yes"
 vim.wo.cursorline = true
 
--- init.lua
+-- Bootstrap do lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Carrega os plugins
 require("lazy").setup("plugins")
 require("plugins.snacks")
-
